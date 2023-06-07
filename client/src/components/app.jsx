51b2acc193e1c7355121ae352258c7a69b05a610
Matthew Baseman <matthew.baseman@gmail.com>
2023-06-06 19:50:09 -0700

    Map with point and Register and Sign In buttons being developed

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,12 +2,30 @@ import React, { useState, useEffect } from 'react';
 import Map from './map.jsx'
 
 const App =() => {
-  const [view, setView] = useState()
+  const [view, setView] = useState("default")
   const [address, setAddress] = useState(null)
+
+  const renderView = () => {
+    switch (view) {
+      case "default":
+        return (
+          <div>
+            <header>
+              <nav>
+                <h1>Register</h1>
+                <h1>Login</h1>
+              </nav>
+            </header>
+            <Map />
+          </div>
+        );
+
+    }
+  }
 
   return (
     <div className="App">
-      <Map />
+      {renderView()}
     </div>
   )
 }
